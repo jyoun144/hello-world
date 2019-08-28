c11197db368684dce2975fd8fc6a5764476de912
young <young@10.216.50.24>
2019-08-28 14:24:54 -0400

    Second Revision

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,8 +1,23 @@
 package test;
+import java.util.Random;
 
 public class HelloWorld {
+	private static Random random = new Random();
+	private static float aFreq = 0.25f;
+	private static float gFreq = 0.25f;
+	private static float tFreq = 0.25f;
+	private static float cFreq = 0.25f;
+	
 
 	public static void main(String[] args) {
-		System.out.println("H");
+		for(int i=0; i<5; i++)
+		{
+		System.out.println(GetNucleoTide());
+		}
+	}
+	
+	private static float GetNucleoTide()
+	{
+		return random.nextFloat();
 	}
 }
