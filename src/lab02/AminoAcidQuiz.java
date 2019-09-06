3effd38bb459284805aa3b586c74926f91a3540c
young <young@10.216.77.159>
2019-09-06 12:53:47 -0400

    Updated Lab02

--- src/lab02/AminoAcidQuiz.java
+++ src/lab02/AminoAcidQuiz.java
@@ -1,7 +1,7 @@
 package lab02;
 import java.util.Random;
 
-public class CodonGeneratorUniform {	
+public class AminoAcidQuiz {	
 	private final static String[] SHORT_NAMES = 
 		{ "A","R", "N", "D", "C", "Q", "E", 
 		"G",  "H", "I", "L", "K", "M", "F", 
@@ -17,14 +17,31 @@ public class CodonGeneratorUniform {
 		"phenylalanine", "proline", 
 		"serine","threonine","tryptophan", 
 		"tyrosine", "valine"};
+		
 
 	public static void main(String[] args) 
-	{		
-		String [][] aa = getAminoAcids();
-		int b = 1;
-	      
+	{
+		Random random = new Random();
+		int totalTestTimeInput = 30;
+		long startTime = System.currentTimeMillis();
+		String [][] aminoAcidSet = getAminoAcids();
+		giveTest(totalTestTimeInput, aminoAcidSet, random);
 		
-	}	
+		int r = 1;		
+		
+	}
+	private static void giveTest(int totalTestTime, String [][] aminoAcidSet, Random random)
+	{
+		boolean isCorrectAnswer = false;
+		boolean isTargetTimeElapsed = false;
+		do {
+			String [][] target = getTargetAminoAcid(random, aminoAcidSet);
+			 System.out.println("Enter single letter system for " + target[0][1]);
+			String input = System.console().readLine().toUpperCase();
+			   // Statements
+			}while(isCorrectAnswer == true && isTargetTimeElapsed == true);	
+	}
+	
 	
 	private static void printFinalReport(int totalSequenceCount, int targetCodonCount, String targetCodon)
 	{
@@ -40,17 +57,20 @@ public class CodonGeneratorUniform {
 	}
 	private static String [][] getAminoAcids()
 	{			
-			int length = SHORT_NAMES.length;
-			
+			int length = SHORT_NAMES.length;			
 			String[][] aminoAcids = new String[length][2];
 			for(int i=0; i<length;i++)
 			{
 				aminoAcids[i][0] = SHORT_NAMES[i];
 				aminoAcids[i][1] = FULL_NAMES[i];
 			}
-			
-			return aminoAcids;
-			
-		
+			return aminoAcids;				
 	}
+	private static String [][] getTargetAminoAcid(Random rand, String [][] aminoAcids)
+	{
+		int index = rand.nextInt(aminoAcids.length);		
+		String [][] target = new String[][] {{aminoAcids[index][0],aminoAcids[index][1]}};
+		return target;		
+		
+	}	
 }
