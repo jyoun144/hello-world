a223cee66db9694bb3a4478aaea670faeb82bad2
young <young@LAPTOP-PFBDSGGD>
2019-10-23 00:38:05 -0400

    Additional Changes

--- src/lab05/Button1.java
+++ src/lab05/Button1.java
@@ -7,29 +7,46 @@ import java.awt.event.*;
 public class Button1 extends JFrame
 {	
 	private static final long serialVersionUID = -2082381602987081784L;	
-	private JComboBox<String> cb = new JComboBox<>();	
+	private JComboBox<String> cb = new JComboBox<>();
+	private static String STRIKE_TITLE = "Add Strike";
+	private static String SEPARATOR_TITLE = "Add Separator";
+	private static String SEPARATOR = "***********************";
+	private String[] lst = new String[] {"Punch:Jab", "Punch:Cross", "Punch:Hook", "Punch:Upper Cut", "Punch:Overhand",
+											"Kick:Round", "Kick:Front", "Kick:Side", "Kick:Reverse", "Kick:Switch"};
 	private JButton	
-	b1 = new JButton("Button 1"),
-	b2 = new JButton("Button 2");
+	b1 = new JButton(STRIKE_TITLE),
+	b2 = new JButton(SEPARATOR_TITLE);
+
 	private JTextArea txt = new JTextArea(20,40);
 	private ActionListener al = new ActionListener()
 			{
 				public void actionPerformed(ActionEvent e)
 				{
-					String name = ((JButton)e.getSource()).getText();
-					String selectedItem = cb.getSelectedItem().toString();
-					txt.append(selectedItem + "\n");					
+					String currentText;
+					if(((JButton)e.getSource()).getText().equals(STRIKE_TITLE))
+					{
+						currentText = cb.getSelectedItem().toString();
+					}
+					else
+					{
+						currentText = SEPARATOR;
+					}
+					txt.append(currentText + "\n");					
 				}
 			};
 	public Button1()
-	{
-		cb.addItem("Punch:  Jab");
+	{		
+		for(String item : lst)	
+		{
+			cb.addItem(item);
+		}		
 		b1.addActionListener(al);
 		b2.addActionListener(al);
 		this.setLayout(new FlowLayout());
 		this.add(b1);
 		this.add(b2);
-		this.add(cb);
+		this.add(cb);		
 		this.add(new JScrollPane(txt));
+		this.setSize(400,300);
 	}
 }
