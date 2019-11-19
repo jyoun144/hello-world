7dcd8fd40d536dc0b92d7f0271787d9c0f97121b
young <young@LAPTOP-PFBDSGGD>
2019-11-18 19:15:05 -0500

    Updated text for initial screen

--- src/lab08/Constants.java
+++ src/lab08/Constants.java
@@ -10,7 +10,7 @@ public class Constants
 	public static final String ERROR_MSG_NUM_FORMAT = "******NUMBER FORMAT EXCEPTION*****\n*****ENTER A VALID WHOLE NUMBER*****";
 	public static final String ERROR_MSG_GENERAL = "******GENERAL SYSTEM EXCEPTION*****\n*****ENTER A VALID WHOLE NUMBER*****";
 	public static final String ERROR_MSG_INVALID_RANGE = "******INVALID NUMBER ENTRY*****\n*****ENTER A WHOLE NUMBER BETWEEN 2 AND 9223372036854775807*****";
-	public static final String INFO_MSG_INITIAL = "1. Input a whole number between 2 and 9,223,372,036,854,775,807 \n into the text box with a blue border." + 
+	public static final String INFO_MSG_INITIAL = "1. Input a whole number between 2 and 9,223,372,036,854,775,807 \n into the text box with the blue border." + 
 			                                       "\n\n2. Click the 'Start' button to calculate the number of prime \nnumbers between 2 and the input whole number." +
-			                                       "\n\n3. Select option 1, 5, 10, 15 or 20 to set the number of worker threads.";
+			                                       "\n\n3. Select option 1, 2, 3, 4 or 8 in order to set the thread pool size.";
 }
